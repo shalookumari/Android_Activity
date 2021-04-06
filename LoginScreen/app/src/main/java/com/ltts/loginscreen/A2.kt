509a0db4e3737fc1c7659f39ee70dab23f378507
shalookumari <78845430+shalookumari@users.noreply.github.com>
2021-04-06 17:43:15 +0530

    activity

---
+++ LoginScreen/app/src/main/java/com/ltts/loginscreen/A2.kt
@@ -0,0 +1,11 @@
+package com.ltts.loginscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class A2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a2)
+    }
+}
